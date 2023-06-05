f28f363a2de23f8515a4f14432c4964c89209ab5
Luc PHAN <luc-phan@users.noreply.github.com>
2023-06-05 18:53:49 +0200

    feat: add todo creation

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from urllib.parse import urljoin
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
 import requests
 
 from settings import WEB_PORT, API_URL
@@ -19,10 +20,32 @@ def list_(request):
     return dict(todos=response.json())
 
 
+@view_config(
+    route_name='new',
+    request_method='GET',
+    renderer='templates/new.jinja2'
+)
+def new(request):
+    return {}
+
+
+@view_config(
+    route_name='create',
+    request_method='POST',
+)
+def create(request):
+    title = request.params['title']
+    requests.post(TODO_API_URL, json=dict(title=title))
+    url = request.route_url('list') 
+    return HTTPFound(location=url)
+
+
 if __name__ == '__main__':
     with Configurator() as config:
         config.include('pyramid_jinja2')
         config.add_route('list', '/')
+        config.add_route('new', '/new')
+        config.add_route('create', '/create')
         config.scan()
         app = config.make_wsgi_app()
 
